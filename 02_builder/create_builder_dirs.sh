400cf05f17be1dc50cee898359120dab8e05fc69
BugbearR <bugbearr@bugbearr.jp>
2024-03-01 13:15:50 +0900

    Add electron forge template sample.

---
+++ 02_builder/create_builder_dirs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+npm init electron-app@latest 02_01_webpack -- --template=webpack
+npm init electron-app@latest 02_02_webpack-ts -- --template=webpack-typescript
+npm init electron-app@latest 02_03_vite -- --template=vite
+npm init electron-app@latest 02_04_vite-ts -- --template=vite-typescript
